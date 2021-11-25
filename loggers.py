719608ccd439a2afb5b5dfc175566ecc79c1110a
Michal Mikolas <nanuqcz@gmail.com>
2021-11-25 12:04:05 +0100

    Added ConsoleLogger.

--- loggers.py
+++ loggers.py
@@ -1,14 +1,21 @@
+from datetime import datetime
+import logging
+import traceback
+
 
 class Logger():
-	def log(msg):
+	def log(self, msg:str):
 		raise NotImplementedError
+
+	def now_str(self) -> str:
+		return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 
 class FakeLogger(Logger):
-	def log(msg):
+	def log(self, msg:str):
 		pass
 
 
 class ConsoleLogger(Logger):
-	def log(msg):
-		pass
+	def log(self, msg:str):
+		print("[{:s}] {:s}".format(self.now_str(), msg))
